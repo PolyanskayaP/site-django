98143edc3b1ec537a9c4f90dfccd9a5365cc9493
Полина Полянская <ppolina353@yandex.ru>
2022-07-15 15:44:05 +0300

    commit

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views    #из этой . папки импорт вьюс 
 
 urlpatterns = [
-    path('', views.index),       #при переходе на глав стр вызывается функция индекс из файла вьюс (сами создали)
-    path('about-us', views.about)
+    path('', views.index, name ='home'),       #при переходе на глав стр вызывается функция индекс из файла вьюс (сами создали)
+    path('about', views.about, name='about')  #name сами прописали 
 ]
